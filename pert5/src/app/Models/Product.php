c0a3374d53f0adddf6b19ead5c7f91f03d0fbc21
radith001 <radit.1126@gmail.com>
2025-04-28 19:01:16 +0700

    gas terus

---
+++ pert5/src/app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use GuzzleHttp\Client;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+    protected $fillable = [
+        'name',
+        'price',
+        'client_id',
+    ];
+
+    public function client() {
+        return $this->belongsTo(Client::class);
+    }
+}
